8936e10492fe846c86e8747567104a328e959dab
Sining Li <lisining@gmail.com>
2021-01-17 19:16:10 +0800

    MOD: 无法成功实现一个包含引用的闭包...

--- bst/src/main.rs
+++ bst/src/main.rs
@@ -1,4 +1,5 @@
 // reference 1 : https://stackoverflow.com/questions/54613966/error-reached-the-recursion-limit-while-instantiating-funcclosure
+// reference 2 : https://stevedonovan.github.io/rustifications/2018/08/18/rust-closures-are-hard.html
 
 struct TreeNode {
     val: u64,
@@ -70,6 +71,29 @@ impl Tree {
             None => {}
         }
     }
+
+    // 需要加上 Copy Trait 的限定！否则会报错
+    fn traverse2<F: FnMut(u64)>(&self, mut cb: F ) {
+        if let Some(tn) = &self.root {
+            Tree::traverse_inner2(tn, cb);
+        }
+        // cb (1);
+        // cb (2);
+        // cb (3);
+    }
+
+    // 需要加上 Copy Trait 的限定！否则会报错
+    fn traverse_inner2 (tn: &TreeNode, mut cb: impl FnMut(u64) ) {
+        // if let Some(tleft) = &tn.left {
+        //     Tree::traverse_inner2(tleft, cb);
+        // };
+        // println!("calling ");
+        cb (tn.val);
+
+        // if let Some(tright) = &tn.right {
+        //     Tree::traverse_inner2(tright, cb);
+        // };
+    }
 }
 
 fn main() {
@@ -79,5 +103,10 @@ fn main() {
     t.insert(2);
     t.insert(6);
 
-    t.traverse(|x| { println!("{}", x)})
+    // t.traverse(|x| { println!("{}", x)});
+
+    let mut sum = 0;
+    // 需要加上 move，否则会报错
+    t.traverse2(Box::new(|x| { sum +=x; println!("add {} produces {}", x, sum);}));
+    println!("final sum = {}", sum);
 }
